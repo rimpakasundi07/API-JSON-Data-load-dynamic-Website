750d2dc1f9b4db381d98445862c7c0a11a82ecf6
Rimpa Kasundi <rimpakasundi@gmail.com>
2025-09-05 21:52:14 +0530

    Add fetch API implementation for JSON Placeholder

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-console.log("Explore API");
-
-const person = {
-  name: "Nikita",
-  fruit: "Strawberry",
-  dish: "mutton thali",
-  friends: ["Soniya", "Riya", "Keya"],
-  isRich: true,
-  money: 34000,
+const loadData = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    // promise of response
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 };
 
-console.log(person, typeof person);
+const loadpost = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
 
-// JSON -> JS object with Notation
-// JSON.stringify -> JSON
-// JSON.parse -> object
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      displayPost(json);
+    });
+};
 
-const personJSON = JSON.stringify(person);
-console.log(personJSON, typeof personJSON);
+const displayPost = (posts) => {
+  posts.forEach((post) => {
+    console.log(post);
+  });
+};
 
-const parseJSON = JSON.parse(personJSON);
-console.log(parseJSON);
+// ---------------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>.
+
+// console.log("Explore API");
+
+// const person = {
+//   name: "Nikita",
+//   fruit: "Strawberry",
+//   dish: "mutton thali",
+//   friends: ["Soniya", "Riya", "Keya"],
+//   isRich: true,
+//   money: 34000,
+// };
+
+// console.log(person, typeof person);
+
+// * JSON -> JS object with Notation
+// * JSON.stringify -> JSON
+// * JSON.parse -> object
+
+// const personJSON = JSON.stringify(person);
+// console.log(personJSON, typeof personJSON);
+
+// const parseJSON = JSON.parse(personJSON);
+// console.log(parseJSON);
+
+// -------------------------------------->>>>>>>>>>>>>>>>>>>>>>>
